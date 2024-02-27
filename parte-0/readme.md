d3c9a7dbde4086c01009e4cf0d6d0eb4cb6aa28e
Jonathan Barsky <barskyjonathan96@gmail.com>
2024-02-27 12:34:16 -0300

    parte 1

---
+++ parte-0/readme.md
@@ -0,0 +1,1 @@
+agregar aca en formato md los ejercicios de la parte cero
---
+++ parte1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message message="Esta es un" />
+      <Message message="mensaje" />
+      <Message message="Extraordinario" />
+    </div>
+  );
+}
+
+export default App;
+  
